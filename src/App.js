b438264fdb2496b9e84beaf560e0a54e242b435a
viniciussoterocaio <vinicaio2016@gmail.com>
2024-04-27 22:27:29 -0300

    ajuste para deploy

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './imgs/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bem Vindo ao Easy Sites Brasil venha fazer Seu site.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
